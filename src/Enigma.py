5e95c86ff5dc3f68e396b77c6a96feae711d0721
maximatey <maximun154@gmail.com>
2023-07-09 23:00:56 +0700

    added main code

---
+++ src/Enigma.py
@@ -0,0 +1,159 @@
+import string
+
+class EnigmaM3:
+    def __init__(self, rotor_config, rotor_positions, ring_settings):
+        self.alphabet = string.ascii_uppercase
+        self.rotors = {}
+        self.position = rotor_positions
+        self.rings = ring_settings
+        self.reflector = {
+            'A': 'Y', 'B': 'R', 'C': 'U', 'D': 'H', 'E': 'Q', 'F': 'S', 'G': 'L', 'H': 'D', 'I': 'P',
+            'J': 'X', 'K': 'N', 'L': 'G', 'M': 'O', 'N': 'K', 'O': 'M', 'P': 'I', 'Q': 'E', 'R': 'B',
+            'S': 'F', 'T': 'Z', 'U': 'C', 'V': 'W', 'W': 'V', 'X': 'J', 'Y': 'A', 'Z': 'T'
+        }
+        self.turnovers = {
+            'I': 'Q', 'II': 'E', 'III': 'V'
+        }
+        self.plugboard = {}
+        for rotor, wiring in rotor_config.items():
+            self.rotors[rotor] = wiring
+        
+    def rotate(self):
+        self.position['III'] = self.alphabet[(self.alphabet.index(self.position['III']) + 1) % 26]
+        if self.position['III'] == self.turnovers['III']:
+            self.position['II'] = self.alphabet[(self.alphabet.index(self.position['II']) + 1) % 26]
+            if self.position['II'] == self.turnovers['II']:
+                self.position['I'] = self.alphabet[(self.alphabet.index(self.position['I']) + 1) % 26]
+    
+    def encrypt(self, char):
+        if char not in self.alphabet:
+            return char
+        print()
+        self.rotate()
+        print("Current Rotor Position:\nLeft: " + self.position['I'] + "\nMiddle: " + self.position['II'] + "\nRight: " + self.position['III'])
+        print()
+        print("Input character: " + char)
+        
+        char = self.plugboard.get(char, char)
+        print("Enkripsi Plugboard: " + char)
+        
+        char = self.rotors['III'][(self.alphabet.index(char) + self.alphabet.index(self.position['III'])) % 26]
+        print("Enkripsi rotor kanan: " + char)
+        char = self.rotors['II'][(self.alphabet.index(char) + self.alphabet.index(self.position['II'])) % 26]
+        print("Enkripsi rotor tengah: " + char)
+        char = self.rotors['I'][(self.alphabet.index(char) + self.alphabet.index(self.position['I'])) % 26]
+        print("Enkripsi rotor kiri: " + char)
+        char = self.reflector[char]
+        print("Enkripsi reflector: " + char)
+        char = self.alphabet[(self.rotors['I'].index(char) - self.alphabet.index(self.position['I'])) % 26]
+        print("Enkripsi rotor kiri: " + char)
+        char = self.alphabet[(self.rotors['II'].index(char) - self.alphabet.index(self.position['II'])) % 26]
+        print("Enkripsi rotor tengah: " + char)
+        char = self.alphabet[(self.rotors['III'].index(char) - self.alphabet.index(self.position['III'])) % 26]
+        print("Enkripsi rotor kanan: " + char)
+        char = self.plugboard.get(char, char)
+        print("Enkripsi Plugboard: " + char)
+        print()
+        
+        return char
+    
+    def set_rotor_position(self, rotor, position):
+        self.position[rotor] = position
+    
+    def set_ring_setting(self, rotor, setting):
+        self.rings[rotor] = setting
+    
+    def add_plug(self, char1, char2):
+        if char1 not in self.plugboard.values() and char2 not in self.plugboard.values():
+            self.plugboard[char1] = char2
+            self.plugboard[char2] = char1
+            print("Plug added: " + char1 + "-" + char2)
+        else:
+            print("Plug already exists!")
+
+    def print_positions(self):
+        print()
+        print("Current Rotor Positions:")
+        
+        print("1. Left Rotor: " + self.position['I'])
+        print("2. Middle Rotor: " + self.position['II'])
+        print("3. Right Rotor: " + self.position['III'])
+        print()
+            
+    def remove_plug(self, char):
+        if char in self.plugboard:
+            char2 = self.plugboard[char]
+            del self.plugboard[char]
+            del self.plugboard[char2]
+    
+    def set_initial_position(self, initial_positions):
+        for rotor, position in initial_positions.items():
+            self.set_rotor_position(rotor, position)
+    
+    def set_initial_ring_setting(self, initial_settings):
+        for rotor, setting in initial_settings.items():
+            self.set_ring_setting(rotor, setting)
+    
+    def encrypt_text(self, text):
+        text = text.upper()
+        encrypted_text = ''
+        
+        for char in text:
+            encrypted_text += self.encrypt(char)
+        
+        return encrypted_text
+    
+    def decrypt_text(self, text):
+        text = text.upper()
+        decrypted_text = ''
+        
+        for char in text:
+            decrypted_text += self.encrypt(char)
+        
+        return decrypted_text
+    
+    def get_rotor(self, id):
+        return self.rotors[id]
+    
+    def set_rotor(self, rotor_id, rotor_string):
+        self.rotors[rotor_id] = rotor_string
+        
+    def print_plugboard(self):
+        print("Plugboard Configuration:")
+        for char1, char2 in self.plugboard.items():
+            print(f"{char1}-{char2}")
+
+# Inisialisasi Enigma dengan konfigurasi rotor dan posisi awal rotor
+# rotor_config = {
+#     'I': 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+#     'II': 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+#     'III': 'BDFHJLCPRTXVZNYEIWGAKMUSQO'
+# }
+
+# rotor_positions = {
+#     'I': 'A',
+#     'II': 'A',
+#     'III': 'A'
+# }
+
+# enigma = EnigmaM3(rotor_config, rotor_positions, rotor_positions)
+
+# # Set posisi awal rotor
+# initial_positions = {
+#     'I': 'A',
+#     'II': 'A',
+#     'III': 'Z'
+# }
+
+# enigma.set_initial_position(initial_positions)
+
+# # Enkripsi teks
+# plaintext = "A"
+# encrypted_text = enigma.encrypt_text(plaintext)
+# print("Teks terenkripsi:", encrypted_text)
+# print("="*50)
+# enigma = EnigmaM3(rotor_config, rotor_positions, rotor_positions)
+# enigma.set_initial_position(initial_positions)
+# # Dekripsi teks
+# decrypted_text = enigma.decrypt_text(encrypted_text)
+# print("Teks terdekripsi:", decrypted_text)
---
+++ src/main.py
@@ -0,0 +1,226 @@
+from Enigma import EnigmaM3
+import string
+
+rotor_config = {
+    'I': 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
+    'II': 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
+    'III': 'BDFHJLCPRTXVZNYEIWGAKMUSQO'
+}
+
+alphabet = string.ascii_uppercase
+
+rotor_positions = {
+    'I': 'A',
+    'II': 'A',
+    'III': 'A'
+}
+
+enigma = EnigmaM3(rotor_config, rotor_positions, rotor_positions)
+
+# Set posisi awal rotor
+initial_positions = {
+    'I': 'A',
+    'II': 'A',
+    'III': 'Z'
+}
+
+enigma.set_initial_position(initial_positions)
+
+# # Enkripsi teks
+# plaintext = "A"
+# encrypted_text = enigma.encrypt_text(plaintext)
+# print("Teks terenkripsi:", encrypted_text)
+
+# enigma = EnigmaM3(rotor_config, rotor_positions, rotor_positions)
+# enigma.set_initial_position(initial_positions)
+# # Dekripsi teks
+# decrypted_text = enigma.decrypt_text(encrypted_text)
+# print("Teks terdekripsi:", decrypted_text)
+
+print("Welcome to Virtual Enigma Machine!")
+print("Catatan : Karena enkripsi dan dekripsi mesin enigma melewati proses yang sama, gunakan opsi 2 atau 3 untuk melakukan enkripsi dan/atau dekripsi.")
+print("=" * 50)
+
+option = -1
+settingOption = -1
+optionText = "Pilih Nomor Opsi yang ingin digunakan! (1/2/3) \n1. Setting Mesin\n2. Input per huruf\n3. Input kalimat\n999. Keluar"
+settingText = "1. Setting rotor\n2. Setting plugboard\n3. Setting Rotor Position\n999. Kembali"
+rotorListText = "Berikut list rotor yang tersedia:\n1. Rotor I : EKMFLGDQVZNTOWYHXUSPAIBRCJ \n2. Rotor II : AJDKSIRUXBLHWTMCQGZNPYFVOE\n3. Rotor III: BDFHJLCPRTXVZNYEIWGAKMUSQO"
+while True:
+    option = -1
+    
+    print("Current configs: ")
+    print()
+    print("Current rotors :\n1. Rotor kiri: " + enigma.get_rotor('I') + "\n2. Rotor tengah: " + enigma.get_rotor('II') + "\n3. Rotor kanan: "+ enigma.get_rotor('III'))
+    print()
+    enigma.print_plugboard()
+    print()
+    enigma.print_positions()
+    print()
+    
+    print(optionText)
+    option = input("Pilihan: ")
+    print("=*" * 25)
+    if option == '1':
+        while True:
+            print(settingText)
+            settingOption = input("Pilihan: ")
+            print("=*" * 25)
+            if settingOption == '1':
+                print("Current rotors :\n1. Rotor kiri: " + enigma.get_rotor('I') + "\n2. Rotor tengah: " + enigma.get_rotor('II') + "\n3. Rotor kanan: "+ enigma.get_rotor('III') + "\n999. Kembali")
+                print("Pilih rotor yang ingin diubah")
+                opsiUbahRotor = -1
+                opsiUbahRotor = input()
+                if opsiUbahRotor == '999':
+                    break
+                elif opsiUbahRotor == '1':
+                    print(rotorListText)
+                    opsiGear = input("Pilihan (I/II/III): ")
+                    while not (opsiGear != 'I' or opsiGear != 'II' or opsiGear != 'III'):
+                        print("Invalid input, silahkan input kembali")
+                        opsiGear = input("Pilihan: ")
+                    enigma.set_rotor('I', rotor_config[opsiGear])
+                elif opsiUbahRotor == '2':
+                    print(rotorListText)
+                    opsiGear = input("Pilihan (I/II/III): ")
+                    while not (opsiGear != 'I' or opsiGear != 'II' or opsiGear != 'III'):
+                        print("Invalid input, silahkan input kembali")
+                        opsiGear = input("Pilihan: ")
+                    enigma.set_rotor('II', rotor_config[opsiGear])
+                elif opsiUbahRotor == '3':
+                    print(rotorListText)
+                    opsiGear = input("Pilihan (I/II/III): ")
+                    while not (opsiGear != 'I' or opsiGear != 'II' or opsiGear != 'III'):
+                        print("Invalid input, silahkan input kembali")
+                        opsiGear = input("Pilihan: ")
+                    enigma.set_rotor('III', rotor_config[opsiGear])
+                else:
+                    print("Invalid Input!")
+            elif settingOption == '2':
+                print("Current Plugboard:\n")
+                enigma.print_plugboard()
+                print("1. Add plug\n2. Remove plug\n999. Kembali")
+                print("Pilihan: ")
+                optionPlug = -1
+                optionPlug = input()
+                if optionPlug=='1':
+                    plug1 = input("Plug 1 :").upper()
+                    plug2 = input("Plug 2 :").upper()
+                    
+                    while (plug1 not in alphabet) or (plug2 not in alphabet):
+                        print("Invalid inputs, please try again")
+                        plug1 = input("Plug 1 :").upper()
+                        plug2 = input("Plug 2 :").upper()
+                    
+                    enigma.add_plug(plug1,plug2)
+                elif optionPlug == '2':
+                    plug1 = input("Plug (Choose either character) :").upper()
+                    
+                    while (plug1 not in alphabet) :
+                        print("Invalid inputs, please try again")
+                        plug1 = input("Plug (Choose either character) :").upper()
+                        
+                    enigma.remove_plug(plug1)
+                elif optionPlug == '999':
+                    break
+                else: 
+                    print("Invalid Input!")
+            elif settingOption == '3':
+                enigma.print_positions()
+                print("Pilihan (1/2/3)")
+                posOption = input()
+                if posOption == '1':
+                    print("Pilih posisi rotor (A-Z) :")
+                    poschar = input().upper()
+                    while poschar not in alphabet:
+                        print("Invalid Input!")
+                        print("Pilih posisi rotor (A-Z) :")
+                        poschar = input().upper()
+                    enigma.set_rotor_position('I',poschar)
+                    print("\nRotor I has set to position "  + poschar + "\n")
+                elif posOption == '2':
+                    print("Pilih posisi rotor (A-Z) :")
+                    poschar = input().upper()
+                    while poschar not in alphabet:
+                        print("Invalid Input!")
+                        print("Pilih posisi rotor (A-Z) :")
+                        poschar = input().upper()
+                    enigma.set_rotor_position('II',poschar)
+                    print("\nRotor II has set to position "  + poschar + "\n")
+                elif posOption == '3':
+                    print("Pilih posisi rotor (A-Z) :")
+                    poschar = input().upper()
+                    while poschar not in alphabet:
+                        print("Invalid Input!")
+                        print("Pilih posisi rotor (A-Z) :")
+                        poschar = input().upper()
+                    enigma.set_rotor_position('III',poschar)
+                    print("\nRotor III has set to position "  + poschar + "\n")
+                else :
+                    print("Invalid input!")
+            elif settingOption == '999':
+                break
+            else:
+                print("Invalid Input!")
+                print()
+    elif option == '2':
+        intext = ""
+        outtext = ""
+        print("INPUT 999 UNTUK KELUAR")
+        while True:
+            char = input("Input:").upper()
+            if char in alphabet:
+                intext = intext + char
+                char2 = enigma.encrypt(char)
+                outtext = outtext + char2
+                print("Output: " + char2)
+                print()
+                print("Input text: " + intext)
+                print("Output text: " + outtext)
+            elif char == '999':
+                break
+            else:
+                print("Invalid input")
+    elif option == '3':
+        intext = ""
+        outtext = ""
+        valid = True
+        print("Input kalimat yang ingin dienkripsi/dekripsi")
+        intext = input().upper()
+        
+        for char in intext:
+            if char not in alphabet:
+                valid = False
+        
+        while not valid:
+            print("Invalid input")
+            print()
+            print("Input kalimat yang ingin dienkripsi/dekripsi")
+            intext = input().upper
+            valid = True
+            
+            for char in intext:
+                if char not in alphabet:
+                    valid = False
+                    
+        print("Encrypting: " + intext)
+        outtext = enigma.encrypt_text(intext)
+        print("Output: " + outtext)
+        print()
+        
+    elif option=='999':
+        break
+    else:
+        print("Invalid Input!")
+    print()
+
+print()
+print("Sayonara~")
+        
+        
+                
+                
+                    
+                        
+                    
+    
